f967a7e9431de743c5b584518c9184755b0a080d
sjava <515957966@qq.com>
2023-11-24 17:40:39 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::net::SocketAddr;
+
+use axum::{
+    extract::{Path, Query},
+    response::{Html, IntoResponse},
+    routing::{get, get_service},
+    Router,
+};
+use serde::Deserialize;
+use tower_http::services::ServeDir;
+
+mod error;
+
+#[tokio::main]
+async fn main() {
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+    let name = params.name.as_deref().unwrap_or("World!!!");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+    Html(format!("Hello2 <strong>{name}</strong>"))
+}
---
+++ tests/quick_dev.rs
@@ -0,0 +1,12 @@
+use anyhow::Result;
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    hc.do_get("/hello?name=测试").await?.print().await?;
+    hc.do_get("/hello2/Mike").await?.print().await?;
+    hc.do_get("/src/main.rs").await?.print().await?;
+
+    Ok(())
+}
